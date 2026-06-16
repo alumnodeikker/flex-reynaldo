6ac7ceabac74bd399549ad8b189db0c7aea70515
“yo” <alumnodeikker@gmail.com>
2026-06-16 12:24:14 +0200

    Paso 09 completado

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -26,6 +26,12 @@ const nextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
       {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '57321',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
